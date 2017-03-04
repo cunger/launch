76832408d6ec1e74127e79ed30004a4e40554048
cunger <andrea.christina.unger@gmail.com>
2017-03-04 11:12:42 +0100

    refactoring calculator

--- 101/lesson2/calculator.rb
+++ 101/lesson2/calculator.rb
@@ -1,24 +1,65 @@
-
-def prompt(what_to_prompt, options = "")
-  puts "What's the #{what_to_prompt}? #{options}"
-  gets.chomp
+def prompt(message)
+  puts "=> #{message}"
 end
 
-puts "Welcome to the calculator!"
+def get_answer(message, error_message, &validation)
+  loop do
+    prompt message
+    answer = gets.chomp.strip
+    if validation.call(answer)
+      return answer
+    else
+      prompt error_message
+    end
+  end
+end
 
-number1 = prompt("first number")
-number2 = prompt("second number")
-operation = prompt("operation", "(+, -, *, /)")
+number_validation   = proc { |str| str =~ /^\d+$/ }
+operator_validation = proc { |str| %w(+ - * /).include? str }
+answer_validation   = proc { |str| %w(y n).include? str.downcase }
 
-result = case operation
-         when "+"
-           number1.to_i + number2.to_i
-         when "-"
-           number1.to_i - number2.to_i
-         when "*"
-           number1.to_i * number2.to_i
-         when "/"
-           number1.to_f / number2.to_f
-         end
+prompt "Welcome to the calculator!"
 
-puts "#{number1} #{operation} #{number2} = #{result}"
+loop do
+  # Get numbers and operator from user.
+  number1 = get_answer("What's the first number?",
+                       "Hmm... That doesn't look like a valid number.",
+                       &number_validation)
+  number2 = get_answer("What's the second number?",
+                       "Hmm... That doesn't look like a valid number.",
+                       &number_validation)
+
+  operation_prompt = <<-STR
+    What operation would you like to perform?
+    + (addition)
+    - (subtraction)
+    * (multiplication)
+    / (division)
+  STR
+  operation = get_answer(operation_prompt,
+                         "Please choose +, -, *, or /.",
+                         &operator_validation)
+
+  # Perform calculation.
+  result = case operation
+           when "+"
+             number1.to_i + number2.to_i
+           when "-"
+             number1.to_i - number2.to_i
+           when "*"
+             number1.to_i * number2.to_i
+           when "/"
+             number1.to_f / number2.to_f
+           end
+
+  # Show result.
+  prompt "#{number1} #{operation} #{number2} = #{result}"
+
+  # Ask user whether to perform another calculation.
+  another_one = get_answer("Do you want to perform another operation? y/n",
+                           "Please say 'y' or 'n'.",
+                           &answer_validation)
+  break if another_one != 'y'
+end
+
+prompt "Ciao!"
