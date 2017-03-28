43271a707fed7df3827edb6fd23f6ded2c97c6b6
cunger <andrea.christina.unger@gmail.com>
2017-03-28 10:26:41 +0200

    IP addresses

---
+++ 101/lesson3/hard1.rb
@@ -0,0 +1,11 @@
+
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false if dot_separated_words.size != 4
+
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false unless is_an_ip_number?(word)
+  end
+  true
+end
