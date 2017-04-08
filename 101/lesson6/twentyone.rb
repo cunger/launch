fa8b879bf921d79cd0cfe04b4fe7931703492dbc
cunger <andrea.christina.unger@gmail.com>
2017-04-08 14:42:17 +0200

    refactoring 17+4

--- 101/lesson6/twentyone.rb
+++ 101/lesson6/twentyone.rb
@@ -2,6 +2,10 @@
 CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
 MAX = 21
 
+def deal(deck, hand)
+  hand << deck.pop
+end
+
 def value(hand)
   aces   = hand.select { |card| card == :ace }
   others = hand - aces
@@ -25,31 +29,89 @@ def busted?(hand)
   value(hand) > MAX
 end
 
-def deal(deck, hand)
-  hand << deck.pop
-end
-
-def want_to_stay?
+def want_to?(prompt_msg, trues, falses, error_msg)
   loop do
-    print 'Do you want to hit or stay? '
+    print prompt_msg + ' '
     answer = gets.chomp
-    return true  if answer.downcase == 'stay' || answer.downcase == 's'
-    return false if answer.downcase == 'hit'  || answer.downcase == 'h'
-    puts "Please type 'hit' (or 'h') or 'stay' (or 's')."
+    return true  if trues.include? answer.downcase
+    return false if falses.include? answer.downcase
+    puts error_msg
   end
 end
 
+def want_to_stay?
+  want_to?('Do you want to hit or stay?',
+           %w(stay s),
+           %w(hit h),
+           "Please type 'hit' (or 'h') or 'stay' (or 's').")
+end
+
+def want_to_play_again?
+  puts ''
+  want_to?('Do you want to play again?',
+           %w(yes y),
+           %w(no n),
+           "Please type 'yes' (or 'y') or 'no' (or 'n').")
+end
+
+def show_value(hand, hidden = false)
+  hidden ? '??' : format('%2d', value(hand))
+end
+
+def show_hand(hand, hidden = false)
+  hidden ? "#{hand[0]} ?" : hand.join(' ')
+end
+
 def show(dealer_hand, player_hand, hidden = true)
+  dval = show_value(dealer_hand, hidden)
+  pval = show_value(player_hand)
   system 'clear'
-  puts '----------------------------------------'
-  puts "Dealer (??): #{dealer_hand[0]} ?" if hidden
-  puts "Dealer (#{format('%2d', value(dealer_hand))}): #{dealer_hand.join(' ')}" if !hidden
-  puts "You    (#{format('%2d', value(player_hand))}): #{player_hand.join(' ')}"
-  puts '----------------------------------------'
+  puts '-----------------------------------'
+  puts "Dealer (#{dval}): #{show_hand(dealer_hand, hidden)}"
+  puts "You    (#{pval}): #{show_hand(player_hand)}"
+  puts '-----------------------------------'
   puts ''
 end
 
+def players_turn(deck, dealer_hand, player_hand)
+  loop do
+    break if want_to_stay?
+    deal(deck, player_hand)
+    show(dealer_hand, player_hand)
+    break if busted?(player_hand)
+  end
+  value(player_hand)
+end
+
+def dealers_turn(deck, dealer_hand, player_hand)
+  loop do
+    if value(dealer_hand) < MAX - 4 || value(dealer_hand) < value(player_hand)
+      puts 'Dealer decides to hit.'
+      sleep 1
+      deal(deck, dealer_hand)
+      show(dealer_hand, player_hand, false)
+      break if busted?(dealer_hand)
+    else
+      puts 'Dealer decides to stay.'
+      sleep 1
+      break
+    end
+  end
+  value(dealer_hand)
+end
+
+def announce_winner(dealer_hand, player_hand)
+  puts ''
+  case value(player_hand) <=> value(dealer_hand)
+  when  0 then puts 'It\'s a tie.'
+  when  1 then puts 'You win.'
+  when -1 then puts 'Dealer wins.'
+  end
+end
+
 def game_on
+  # Set up game
+
   deck = CARDS * 4
   deck.shuffle!
 
@@ -63,53 +125,35 @@ def game_on
 
   show(dealer_hand, player_hand)
 
-  player_busted = false
-  dealer_busted = false
+  # Player's turn
 
-  until want_to_stay?
-    deal(deck, player_hand)
-    show(dealer_hand, player_hand)
-    if busted?(player_hand)
-      show(dealer_hand, player_hand, false)
-      puts 'BUSTED!'
-      player_busted = true
-      break
-    end
-  end
+  player_score = players_turn(deck, dealer_hand, player_hand)
 
-  if !player_busted
-    show(dealer_hand, player_hand, false)
-    sleep 1
-    while value(dealer_hand) < MAX - 4 || value(dealer_hand) < value(player_hand)
-      puts 'Dealer decides to hit.'
-      sleep 1
-      deal(deck, dealer_hand)
-      show(dealer_hand, player_hand, false)
-      if busted?(dealer_hand)
-        puts 'BUSTED!'
-        dealer_busted = true
-        break
-      end
-    end
-    puts 'Dealer decides to stay.' unless dealer_busted
-  end
+  show(dealer_hand, player_hand, false)
 
-  puts ''
-  print 'Game over. '
-  case
-  when player_busted
+  if player_score > MAX
+    puts 'BUSTED!'
     puts 'Dealer wins.'
-  when dealer_busted
-    puts 'You win.'
-  when value(player_hand) == value(dealer_hand)
-    puts 'It\'s a tie.'
-  when value(player_hand) > value(dealer_hand)
-    puts 'You win.'
-  when value(player_hand) < value(dealer_hand)
-    puts 'Dealer wins.'
+    return
   end
+
+  # Dealer's turn
+
+  dealer_score = dealers_turn(deck, dealer_hand, player_hand)
+
+  if dealer_score > MAX
+    puts 'BUSTED!'
+    puts 'You win.'
+    return
+  end
+
+  # Game over!
+  announce_winner(dealer_hand, player_hand)
 end
 
-## Let's go!
+## Let's play!
 
-game_on
+loop do
+  game_on
+  break unless want_to_play_again?
+end
