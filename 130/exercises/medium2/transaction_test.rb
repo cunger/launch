b0f1097c8e643081a0c797c566b67c23b5459522
cunger <andrea.christina.unger@gmail.com>
2017-06-16 12:13:34 +0200

    tiny refactoring for consistency

--- 130/exercises/medium2/transaction_test.rb
+++ 130/exercises/medium2/transaction_test.rb
@@ -7,7 +7,7 @@ require_relative 'transaction'
 class TransactionTest < Minitest::Test
 
   def test_paying_enough
-    transaction = Transaction.new 199.0
+    transaction = Transaction.new(199.0)
     input  = StringIO.new("200\n")
     output = StringIO.new
     transaction.prompt_for_payment(input: input, output: output)
@@ -15,10 +15,10 @@ class TransactionTest < Minitest::Test
   end
 
   def test_paying_too_little
-    transaction = Transaction.new 199.0
+    transaction = Transaction.new(199.0)
     input  = StringIO.new("10\n")
     output = StringIO.new
     transaction.prompt_for_payment(input: input, output: output)
-    assert_match(/That is not the correct amount/, output.string)
+    assert_match /That is not the correct amount/, output.string
   end
 end
